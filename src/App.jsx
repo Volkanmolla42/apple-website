273633e05dcfb42bc515c14b9e12cad07407a0c3
Volkan Molla <mollavolkan11@gmail.com>
2024-12-02 21:57:53 +0300

    fix deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import * as Sentry from '@sentry/react'
-import Hero from './components/Hero'
-import Highlights from './components/Highlights'
-import Navbar from './components/Navbar'
-import Model from './components/Model'
-import Features from './components/Features'
-import HowItWorks from './components/HowItWorks'
-import Footer from './components/Footer'
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Navbar from "./components/Navbar";
+import Model from "./components/Model";
+import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <main className='bg-black'>
+    <main className="bg-black">
       <Navbar />
       <Hero />
       <Highlights />
@@ -18,7 +17,7 @@ const App = () => {
       <HowItWorks />
       <Footer />
     </main>
-  )
-}
+  );
+};
 
-export default Sentry.withProfiler(App)
+export default App;
